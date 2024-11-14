4e2b1653c05008fae619f2eab9a5b6e9b3b04449
alusilcof5 <lucia.silva@factoria.org>
2024-11-14 23:22:32 +0100

    first commit

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ window.addEventListener('load', async () => {
   console.log(data);
   const lista = document.createElement('ul');
   
-  data.results.forEach(personaje => {  // Cambié 'character' a 'personaje' para evitar conflicto
-    const elementList = document.createElement('li');  // Cambié el nombre de la variable
+  data.results.forEach(personaje => {  
+    const elementList = document.createElement('li');  
     const name = document.createElement('h2');
     const image = document.createElement('img');
     
-    name.textContent = personaje.name;  // Uso 'personaje' en lugar de 'character'
+    name.textContent = personaje.name;  
     image.src = personaje.image;    
-    elementList.appendChild(name);    // Uso elementoLista en lugar de character
+    elementList.appendChild(name);    
     elementList.appendChild(image);
     lista.appendChild(elementList);
   });
